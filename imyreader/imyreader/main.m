85dffd2e6b439a88e1d86a135dec6de3893d7470
yzx <yang3800650071@163.com>
2013-07-02 21:55:23 +0800

    c

---
+++ imyreader/imyreader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  imyreader
+//
+//  Created by yangzexin on 7/1/13.
+//  Copyright (c) 2013 yangzexin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YXAppDelegate class]));
+    }
+}
